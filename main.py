d0bb79ae1f688c2665fc7485b82b2190ebcbc29a
Андрій <drymik77@gmail.com>
2025-11-03 22:58:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ async def poll_channels():
             for src in SOURCE_CHANNELS:
                 try:
                     entity = await client.get_entity(src)
-                    async for msg in client.iter_messages(entity, limit=2):
+                    async for msg in client.iter_messages(entity, limit=1):
                         if not is_processed(msg.chat_id, msg.id):
                             await forward_message(msg, msg.chat_id)
                 except Exception as e:
